10bb33289a5645e2888b2a03a0e952757d703d73
ChristopheV8 <cristopher.villegas_torres_jr24@student.cart.org>
2025-02-13 15:49:37 -0800

    fesdgsdg

---
+++ java.js
@@ -0,0 +1,27 @@
+let newYearsResolutions = ['Keep a journal', 'Take a falconry class', 'Learn Kubernetes'];
+
+const hobbies = ['your MOM', 'Building things', 'Fixing things'];
+
+const listitem =  hobbies [0];
+
+let seasons = ['Winter', 'Spring', 'Summer', 'Fall'];
+
+let condiments = ['ketchup', 'mayo', 'relish'] ;
+
+condiments = ['mayo'] ;
+
+let untensils = ['Fork', 'Spoon', 'Knife'];
+
+untensils = ['spork'];
+
+let chores = [];
+
+chores.push('dishes', 'laundry');
+
+chores.pop();
+
+const newItemTracker = ['item 0', 'item 1', 'item 2'];
+
+const removed = newItemTracker.pop();
+
++
